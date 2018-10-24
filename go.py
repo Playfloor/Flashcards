107770f4230a855cb140e1811d64f274bd21b898
Ben <ben@klemens.org>
2018-10-23 23:16:51 -0400

    Can't live without a makefile

--- go.py
+++ go.py
@@ -5,8 +5,8 @@ def go(dothis):
     print(dothis)
     return subprocess.getoutput(dothis)
 
-#Files=["abstract-algebra", "analysis", "topology", "category-theory"]
-Files=["category-theory"]
+Files=["abstract-algebra", "topology", "category-theory"]
+#Files=["category-theory"]
 
 [go("pdflatex "+i) for i in Files]
 
@@ -16,6 +16,7 @@ print(str(Count)+" cards.")
 go("rm counts; for i in `seq 1 2 " + Count + "`; do echo $i >> counts; done")
 Pages=go("shuf counts | awk '{print $1 \",\" $1+1}'| tr '\n' ','|sed 's/,$//'")
 
+go("rm -f mid.pdf mid1.pdf")
 go("pdfjam --trim '0in 9.5in 5in 1in' " + "  ".join([" "+i +".pdf " for i in Files]) + " -o mid1.pdf")
 go("pdfjam mid1.pdf \"" + Pages + "\" --no-tidy -o mid.pdf")
 go("pdfcrop --clip mid.pdf cards.pdf")
