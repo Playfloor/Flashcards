fa6dffc13f314c33fdfea9b971b86c14296aa359
Ben <ben@klemens.org>
2018-10-27 12:55:10 -0400

    Elide copyright/attribution cards

--- go.py
+++ go.py
@@ -16,12 +16,13 @@ for i in Ftex:
     go("pdflatex tmp."+i)
 
 Count=go("egrep '(\\\\Dcard|\\\\Card|begin.flashcard)' " + " ".join(Ftex)  + " | wc -l")
+Count=int(Count)-len(Files) #strip the first copyright/attribution card
 print("--------------------\n\t"+str(Count)+" cards.\n--------------------")
 
-go("rm counts; for i in `seq 1 2 " + str(2*int(Count)-2) + "`; do echo $i >> counts; done")
+go("rm counts; for i in `seq 1 2 " + str(2*Count-2) + "`; do echo $i >> counts; done")
 Pages=go("shuf counts | awk '{print $1 \",\" $1+1}'| tr '\n' ','|sed 's/,$//'")
 
 go("rm -f mid.pdf mid1.pdf")
-go("pdfjam --trim '0in 9.5in 5in 1in' " + "  ".join([" tmp."+i +".pdf " for i in Files]) + " -o mid1.pdf")
+go("pdfjam  " + "  ".join([" tmp."+i +".pdf " for i in Files]) + " 3- -o mid1.pdf")
 go("pdfjam mid1.pdf \"" + Pages + "\" --no-tidy -o mid.pdf")
 go("pdfcrop --clip mid.pdf cards.pdf")
